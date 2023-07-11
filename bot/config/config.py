2cc72ec5c489f5945de8637a0c5663d1faf3fa61
alone-hermit <133490910+alone-hermit@users.noreply.github.com>
2023-07-11 23:29:37 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -11,9 +11,9 @@ class Config:
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
 
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://abcd:abcd@cluster0.int0q0q.mongodb.net/?retryWrites=true&w=majority")
     SESSION_NAME = os.environ.get("DATABASE_NAME", "TelegramBot")
 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
     SUDO_USERS = [int(user) for user in (os.environ.get("SUDO_USERS","6230751739 1877279215 5023815012 5906684391 5039863679")).split()]
-    SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/")
+    SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/OFFICIALBOT_SUPPORT")
