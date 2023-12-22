78e0ffc6e59f3d9cc506c832f590fde5e19e2fa8
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-22 18:22:40 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -8,7 +8,7 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "2119246346:AAGrHWRcyCdNlKjcOypDtpkYbDhYTW0E7_Y")
     BOT_NAME = os.environ.get("BOT_NAME", "Aleena")
 
-    API_ID = int(os.environ.get("API_ID", "27589257"))
+    API_ID = int(os.environ.get("API_ID", "15851949"))
     API_HASH = os.environ.get("API_HASH", "f8e386978bf103c4a6baab0a4b92e822")
 
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://bot:bot@cluster0.8vepzds.mongodb.net/?retryWrites=true&w=majority")
