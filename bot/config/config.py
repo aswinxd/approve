fe424f657ec38985f6b3c6c98a3f787903080e3c
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-22 17:44:52 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -8,13 +8,13 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "2119246346:AAGrHWRcyCdNlKjcOypDtpkYbDhYTW0E7_Y")
     BOT_NAME = os.environ.get("BOT_NAME", "Aleena")
 
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", "27589257"))
+    API_HASH = os.environ.get("API_HASH", "2119246346:AAGrHWRcyCdNlKjcOypDtpkYbDhYTW0E7_Y")
 
-    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://abcd:abcd@cluster0.int0q0q.mongodb.net/?retryWrites=true&w=majority")
-    SESSION_NAME = os.environ.get("DATABASE_NAME", "TelegramBot")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://bot:bot@cluster0.8vepzds.mongodb.net/?retryWrites=true&w=majority")
+    SESSION_NAME = os.environ.get("DATABASE_NAME", "Aleena")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
-    SUDO_USERS = [int(user) for user in (os.environ.get("SUDO_USERS","6230751739 1877279215 5023815012 5906684391 5039863679")).split()]
-    SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/OFFICIALBOT_SUPPORT")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001696809539))
+    SUDO_USERS = [int(user) for user in (os.environ.get("SUDO_USERS","")).split()]
+    SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/subotsupport")
   
