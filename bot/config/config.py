ea900cbd35f2c4a3566fb8209bdd807266138b26
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-07 15:34:54 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -17,4 +17,3 @@ class Config:
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002049466903))
   SUDO_USERS = [int(user) for user in (os.environ.get("SUDO_USERS","1137799257")).split()]
   SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/subotsupport")
-~
